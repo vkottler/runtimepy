eff5146cca014382adbc9d14c629ae29d1759786
Vaughn Kottler <vaughnkottler@gmail.com>
2022-11-12 15:24:55 -0600

    0.7.0 - Async task support

--- runtimepy/__init__.py
+++ runtimepy/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=3.1.0
-# hash=3d1b279fd1fe88436fba36035a5675e7
+# hash=05b764368d5303d22b6beadb9216a420
 # =====================================
 
 """
@@ -10,4 +10,4 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "A framework for implementing Python services."
 PKG_NAME = "runtimepy"
-VERSION = "0.6.0"
+VERSION = "0.7.0"
