2323d83bb7f4da0f2b4add3ec5b60e5c4df35bb6
Vaughn Kottler <vaughnkottler@gmail.com>
2023-02-02 23:31:27 -0600

    More work on TCP connection

---
+++ runtimepy/net/mixin.py
@@ -0,0 +1,65 @@
+"""
+Various networking-related class utilities.
+"""
+
+# built-in
+import asyncio as _asyncio
+from typing import Optional as _Optional
+
+# internal
+from runtimepy.net import IpHost as _IpHost
+from runtimepy.net import normalize_host as _normalize_host
+from runtimepy.net.connection import BinaryMessage as _BinaryMessage
+
+
+class BinaryMessageQueueMixin:
+    """A mixin for adding a 'queue' attribute."""
+
+    def __init__(self) -> None:
+        """Initialize this protocol."""
+
+        self.queue: _asyncio.Queue[  # pylint: disable=unsubscriptable-object
+            _BinaryMessage
+        ] = _asyncio.Queue()
+
+
+class TransportMixin:
+    """A class simplifying evaluation of local and remote addresses."""
+
+    _transport: _asyncio.BaseTransport
+
+    def __init__(self, transport: _asyncio.BaseTransport) -> None:
+        """Initialize this instance."""
+
+        self._transport = transport
+
+        # Get the local address of this connection.
+        self.local_address = _normalize_host(
+            *self._transport.get_extra_info("sockname")
+        )
+
+        # A bug in the Windows implementation causes the 'addr' argument of
+        # sendto to be required. Save a copy of the remote address (may be
+        # None).
+        self.remote_address = self._remote_address()
+
+    def _remote_address(self) -> _Optional[_IpHost]:
+        """Get a possible remote address for this connection."""
+
+        result = self._transport.get_extra_info("peername")
+        addr = None
+        if result is not None:
+            addr = _normalize_host(*result)
+        return addr
+
+    def _logger_name(self) -> str:
+        """Get a logger name for this connection."""
+
+        name = str(self.local_address)
+        if self.remote_address is not None:
+            name += f" -> {self.remote_address}"
+        return name
+
+    async def close(self) -> None:
+        """Close this connection."""
+        self._transport.close()
--- runtimepy/net/tcp/connection.py
+++ runtimepy/net/tcp/connection.py
@@ -1,3 +1,57 @@
 """
 A module implementing a TCP connection interface.
 """
+
+# built-in
+from asyncio import Protocol as _Protocol
+from asyncio import Transport as _Transport
+from logging import getLogger as _getLogger
+from typing import Optional as _Optional
+from typing import TypeVar as _TypeVar
+from typing import Union as _Union
+
+# internal
+from runtimepy.net.connection import BinaryMessage as _BinaryMessage
+from runtimepy.net.connection import Connection as _Connection
+from runtimepy.net.mixin import (
+    BinaryMessageQueueMixin as _BinaryMessageQueueMixin,
+)
+from runtimepy.net.mixin import TransportMixin as _TransportMixin
+
+
+class QueueProtocol(_BinaryMessageQueueMixin, _Protocol):
+    """A simple streaming protocol that populates a message queue."""
+
+    def data_received(self, data) -> None:
+        """Handle incoming data."""
+        self.queue.put_nowait(data)
+
+
+T = _TypeVar("T", bound="TcpConnection")
+
+
+class TcpConnection(_Connection, _TransportMixin):
+    """A TCP connection interface."""
+
+    def __init__(self, transport: _Transport, protocol: QueueProtocol) -> None:
+        """Initialize this TCP connection."""
+
+        _TransportMixin.__init__(self, transport)
+
+        # Re-assign with updated type information.
+        self._transport: _Transport = transport
+
+        self._protocol = protocol
+        super().__init__(_getLogger(self._logger_name()))
+
+    async def _await_message(self) -> _Optional[_Union[_BinaryMessage, str]]:
+        """Await the next message. Return None on error or failure."""
+        return await self._protocol.queue.get()
+
+    async def _send_text_message(self, data: str) -> None:
+        """Send a text message."""
+        self._transport.write(data.encode())
+
+    async def _send_binay_message(self, data: _BinaryMessage) -> None:
+        """Send a binary message."""
+        self._transport.write(data)
