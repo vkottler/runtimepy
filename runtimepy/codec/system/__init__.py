d5d1da05ef2aa629955779f03a101734e2991e0c
Vaughn Kottler <vaughnkottler@gmail.com>
2023-08-27 21:40:08 -0500

    Working on typing updates

--- runtimepy/codec/system/__init__.py
+++ runtimepy/codec/system/__init__.py
@@ -3,7 +3,7 @@ A basic type-system implementation.
 """
 
 # built-in
-from typing import Dict
+from typing import Dict, Optional
 
 # third-party
 from vcorelib.namespace import CPP_DELIM, Namespace
@@ -16,6 +16,15 @@ from runtimepy.primitives.byte_order import ByteOrder
 from runtimepy.primitives.type import AnyPrimitiveType, PrimitiveTypes
 
 
+class CustomType:
+    """TODO."""
+
+    def __init__(self, protocol: Protocol) -> None:
+        """Initialize this instance."""
+
+        self.protocol = protocol
+
+
 class TypeSystem:
     """A class for managing a custom type system."""
 
@@ -23,7 +32,7 @@ class TypeSystem:
         """Initialize this instance."""
 
         self.primitives: Dict[str, AnyPrimitiveType] = {}
-        self.custom: Dict[str, Protocol] = {}
+        self.custom: Dict[str, CustomType] = {}
 
         global_namespace = Namespace(delim=CPP_DELIM)
 
@@ -39,23 +48,52 @@ class TypeSystem:
             "ByteOrder", ByteOrder.register_enum(self._enums, name="ByteOrder")
         )
 
-    def register(self, name: str) -> Protocol:
+    def register(self, name: str) -> CustomType:
         """Register a custom type."""
 
-        new_type = Protocol(self._enums)
-        name = self.root_namespace.namespace(name)
-        self.custom[name] = new_type
+        new_type = CustomType(Protocol(self._enums))
+        self.custom[self._name(name, check_available=True)] = new_type
         return new_type
+
+    def _find_name(self, name: str, strict: bool = False) -> Optional[str]:
+        """Attempt to find a registered name."""
+
+        matches = list(self.root_namespace.search(pattern=name))
+
+        assert (
+            0 <= len(matches) <= 1
+        ), f"Duplicate type names! {name}: {matches}"
+
+        assert not strict or matches, f"Name '{name}' not found."
+
+        return matches[0] if matches else None
+
+    def _name(self, name: str, check_available: bool = False) -> str:
+        """Resolve a given name against the current namespace."""
+
+        if check_available:
+            resolved = self._find_name(name)
+            assert (
+                resolved is None
+            ), f"Name '{name}' not available! found '{resolved}'"
+
+        return self.root_namespace.namespace(name)
+
+    def _register_primitive(self, name: str, kind: str) -> None:
+        """TODO."""
+
+        assert name not in self.primitives, name
+        self.primitives[name] = PrimitiveTypes[kind]
 
     def runtime_enum(self, name: str, enum: RuntimeEnum) -> bool:
         """Register an enumeration."""
 
-        name = self.root_namespace.namespace(name)
+        name = self._name(name, check_available=True)
 
         result = self._enums.register(name, enum)
 
-        assert name not in self.primitives, name
-        self.primitives[name] = PrimitiveTypes[enum.primitive]
+        if result:
+            self._register_primitive(name, enum.primitive)
 
         return result
 
@@ -64,16 +102,23 @@ class TypeSystem:
     ) -> None:
         """Register an enumeration."""
 
-        self._enums.enum(name, "int", items=items, primitive=primitive)
+        name = self._name(name, check_available=True)
+
+        enum = self._enums.enum(name, "int", items=items, primitive=primitive)
+        assert enum is not None
+
+        # should this call "runtime_enum" ?
+        # self.runtime_enum(name, )
+
+        self._register_primitive(name, enum.primitive)
 
     def size(self, name: str) -> int:
         """Get the size of a named type."""
 
-        matches = list(self.root_namespace.search(pattern=name))
-        assert len(matches) == 1, f"Duplicate type names {name}: {matches}"
-        name = matches[0]
+        found = self._find_name(name, strict=True)
+        assert found is not None
 
-        if name in self.primitives:
-            return self.primitives[name].size
+        if found in self.primitives:
+            return self.primitives[found].size
 
-        return self.custom[name].array.size
+        return self.custom[found].protocol.array.length()
