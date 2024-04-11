125a2d6e1e53ae3a4cac5a8ae677b16e419d8a6b
Vaughn Kottler <vaughnkottler@gmail.com>
2024-04-11 13:46:17 -0500

    4.0.6 - Small improvements

--- runtimepy/__init__.py
+++ runtimepy/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=3.1.4
-# hash=7c124715ceef27008fa5f5c0185d7249
+# hash=0c50ad9a5f5e687315a8672c2e26ba72
 # =====================================
 
 """
@@ -10,7 +10,7 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "A framework for implementing Python services."
 PKG_NAME = "runtimepy"
-VERSION = "4.0.5"
+VERSION = "4.0.6"
 
 # runtimepy-specific content.
 METRICS_NAME = "metrics"
