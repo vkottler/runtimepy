9c9afd6872f0cd7f69e931cbbb4f4cf0bde953f0
Vaughn Kottler <vaughnkottler@gmail.com>
2024-06-29 18:19:58 -0700

    Improve log rate limiting

--- runtimepy/net/udp/protocol.py
+++ runtimepy/net/udp/protocol.py
@@ -5,10 +5,12 @@ A module implementing a DatagramProtocol for UdpConnection.
 # built-in
 import asyncio as _asyncio
 from asyncio import DatagramProtocol as _DatagramProtocol
+import logging
 from typing import Tuple as _Tuple
 
 # third-party
-from vcorelib.logging import LoggerType as _LoggerType
+from vcorelib.logging import LoggerMixin, LoggerType
+from vcorelib.math import RateLimiter
 
 # internal
 from runtimepy.net.connection import BinaryMessage as _BinaryMessage
@@ -18,7 +20,7 @@ from runtimepy.net.connection import Connection as _Connection
 class UdpQueueProtocol(_DatagramProtocol):
     """A simple UDP protocol that populates a message queue."""
 
-    logger: _LoggerType
+    logger: LoggerType
     conn: _Connection
 
     def __init__(self) -> None:
@@ -29,6 +31,8 @@ class UdpQueueProtocol(_DatagramProtocol):
         ] = _asyncio.Queue()
         self.queue_hwm: int = 0
 
+        self.log_limiter = RateLimiter.from_s(1.0)
+
     def datagram_received(self, data: bytes, addr: _Tuple[str, int]) -> None:
         """Handle incoming data."""
 
@@ -38,7 +42,13 @@ class UdpQueueProtocol(_DatagramProtocol):
     def error_received(self, exc: Exception) -> None:
         """Log any received errors."""
 
-        self.logger.error(exc)
+        LoggerMixin.governed_log(
+            self,  # type: ignore
+            self.log_limiter,
+            "Exception occurred:",
+            level=logging.ERROR,
+            exc_info=exc,
+        )
 
         # Most of the time this error occurs when sending to a loopback
         # destination (localhost) that is no longer listening.
