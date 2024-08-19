b98ce52ff872361be5baf629aaf39d97e3b6bc60
Vaughn Kottler <vaughnkottler@gmail.com>
2024-08-19 13:29:00 -0700

    Improvements

--- tasks/tlm.py
+++ tasks/tlm.py
@@ -5,6 +5,7 @@ A module implementing a simple telemetry sample interface.
 # built-in
 import asyncio
 
+# internal
 from runtimepy.channel.environment.sample import (
     long_name_int_enum,
     sample_bool_enum,
@@ -12,13 +13,13 @@ from runtimepy.channel.environment.sample import (
     sample_float,
     sample_int_enum,
 )
-
-# internal
+from runtimepy.mixins.async_command import AsyncCommandProcessingMixin
 from runtimepy.net.arbiter import AppInfo
 from runtimepy.net.arbiter.struct import (
     TimestampedStruct,
     UdpStructTransceiver,
 )
+from runtimepy.net.arbiter.task import ArbiterTask, TaskFactory
 from runtimepy.net.arbiter.udp import UdpConnectionFactory
 
 
@@ -57,29 +58,57 @@ class SampleTelemetry(UdpConnectionFactory[SampleTelemetryTransceiver]):
     kind = SampleTelemetryTransceiver
 
 
+class SampleTelemetryTask(ArbiterTask, AsyncCommandProcessingMixin):
+    """A sample telemetry task."""
+
+    rx_conn: SampleTelemetryTransceiver
+    tx_conn: SampleTelemetryTransceiver
+
+    async def init(self, app: AppInfo) -> None:
+        """Initialize this task with application information."""
+
+        await super().init(app)
+        self._setup_async_commands()
+
+        kind = SampleTelemetryTransceiver
+
+        # Setup Receive.
+        pattern = "rx"
+        self.rx_conn = app.single(pattern=pattern, kind=kind)
+        assert self.rx_conn.assign_app_rx(pattern, app) is not None
+
+        # Setup transmit.
+        pattern = "tx"
+        self.tx_conn = app.single(pattern=pattern, kind=kind)
+        assert self.tx_conn.assign_app_tx(pattern, app) is not None
+
+    async def dispatch(self) -> bool:
+        """Dispatch an iteration of this task."""
+
+        self.tx_conn.capture()
+
+        # make this commandable
+        flush_count = 10
+
+        if self.metrics.dispatches.value % flush_count == 0:
+            self.tx_conn.capture(sample=False, flush=True)
+
+        return True
+
+
+class SampleTelemetryPeriodic(TaskFactory[SampleTelemetryTask]):
+    """A sample-task application factory."""
+
+    kind = SampleTelemetryTask
+
+
 async def sample_app(app: AppInfo) -> int:
     """Test telemetry sending and receiving."""
 
-    # Setup Receive.
-    pattern = "rx"
-    rx_conn = app.single(pattern=pattern, kind=SampleTelemetryTransceiver)
-    assert rx_conn.assign_app_rx(pattern, app) is not None
-
-    # Setup transmit.
-    pattern = "tx"
-    tx_conn = app.single(pattern=pattern, kind=SampleTelemetryTransceiver)
-    assert tx_conn.assign_app_tx(pattern, app) is not None
-
-    # Do some sending and receiving / polling!
     iteration = 0
     forever = app.config_param("forever", False)
     while not app.stop.is_set() and (iteration < 31 or forever):
-        tx_conn.capture()
         iteration += 1
-
-        if iteration % 10 == 0:
-            tx_conn.capture(sample=False, flush=True)
-
         await asyncio.sleep(0.01)
 
     return 0
