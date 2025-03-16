e124031656f08719ee5f71a05ac257a78e52a432
Vaughn Kottler <vaughn@libre-embedded.com>
2025-03-16 01:22:54 -0500

    5.10.1 - Allow peers to use port variables

--- runtimepy/__init__.py
+++ runtimepy/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=3.1.4
-# hash=a9a53fbeb974140d3611fecadbce5da4
+# hash=649474f8a3122bccd6febc4bb2156b11
 # =====================================
 
 """
@@ -10,7 +10,7 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "A framework for implementing Python services."
 PKG_NAME = "runtimepy"
-VERSION = "5.10.0"
+VERSION = "5.10.1"
 
 # runtimepy-specific content.
 METRICS_NAME = "metrics"
