6f47f3db89beba127e90b2466c32237eed11645b
Vaughn Kottler <vaughn@libre-embedded.com>
2025-04-14 04:29:39 +0000

    5.11.5 - Update for repo move

--- runtimepy/__init__.py
+++ runtimepy/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
-# version=3.1.4
-# hash=e4bd25a4e8683a203d0244d24322fe41
+# version=3.2.0
+# hash=ac222b5cb6e347a1d218135c6b5b25f1
 # =====================================
 
 """
@@ -10,7 +10,7 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "A framework for implementing Python services."
 PKG_NAME = "runtimepy"
-VERSION = "5.11.4"
+VERSION = "5.11.5"
 
 # runtimepy-specific content.
 METRICS_NAME = "metrics"
